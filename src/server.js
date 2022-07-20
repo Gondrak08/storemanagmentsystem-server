ddbfa4054b773899ed51b11d49d7d8d26361570e
Vitor Alecrim <vitor.alecrim.p@gmail.com>
2022-07-20 16:20:04 -0300

    1.1.3

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const http = require('http')
 const app = require('./app')
 const server = http.createServer(app);
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, ()=>console.log(`server running at: http://localhost/${PORT}`))
